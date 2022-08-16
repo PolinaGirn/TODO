c7a5c2cdb073d75b64ddd8dade1af37da7810570
PolinaGirn <lina000805@gmail.com>
2022-08-16 17:58:16 +0700

    Второй урок

--- TODO/TODO/settings.py
+++ TODO/TODO/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+]
 
 # Application definition
 
@@ -41,11 +45,13 @@ INSTALLED_APPS = [
     # main
     'rest_framework',
     'users',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- TODO/frontend/src/App.js
+++ TODO/frontend/src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import UserList from "./components/User";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            'users': []
+        }
+    }
+
+
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/users/').then(response => {
+            this.setState(
+            {
+                'users':response.data
+            }
+        )
+    }).catch(error => console.log(error))
+//        const users = [
+//            {
+//                'username':'fedor007',
+//                'first_name':'Фёдор',
+//                'last_name':'Достаевский',
+//                'email':'fedor@gmail.com',
+//            },
+//            {
+//                'username':'alex777',
+//                'first_name':'Александр',
+//                'last_name':'Грин',
+//                'email':'alex@gmail.com',
+//            }
+//        ]
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Menu menu={this.state.menu}/>
+                <UserList users={this.state.users}/>
+                <Footer footer={this.state.footer}/>
+            </div>
+        )
+    }
 }
 
 export default App;
